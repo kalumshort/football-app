b86d3399920e73492c76837a945fea2c10782011
Kalum <kshorty@live.co.uk>
2019-01-10 20:10:57 +0000

    updated redux store

--- data/store.js
+++ data/store.js
@@ -3,6 +3,6 @@ import thunk from 'redux-thunk';
 import reducer from './reducer';
 import initialState from './initial';
 
-const store = createStore( reducer, initialState, applyMiddleware(thunk));
+const store = createStore( reducer, initialState,);
 
 export default store;
